d8b72676488e0746de56f99e041a6c753a5c117b
Aida Legrand <celebritas@yandex.ru>
2015-12-18 10:52:58 -0800

    fix urls

--- client/files/userscript.js
+++ client/files/userscript.js
@@ -2,7 +2,7 @@
 var user = angular.module('user', ['ngRoute']);
 user.config(function ($routeProvider){
 	$routeProvider
-	.when('/', {templateUrl: 'http://localhost:8000/files/user.html'})
+	.when('/', {templateUrl: 'http://mediaaboutus.aidalegrand.com/files/user.html'})
 	.otherwise({redirectTo: '/'})
 });
 user.controller('UserController', function($scope){
